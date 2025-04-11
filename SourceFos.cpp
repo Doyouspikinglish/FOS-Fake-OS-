19fcfba07ddefa9478eee14ce471670cf52eb5dd
Doyouspikinglish <kaanbekdas8@gmail.com>
2025-04-11 19:26:52 +0200

    Update SourceFos.cpp
    rewrote the entire code for the FOS. new commands, languages will be in 0.a2

--- SourceFos.cpp
+++ SourceFos.cpp
@@ -1,203 +1,178 @@
 #include <iostream>
+#include <stdio.h>
 #include <Windows.h>
-#include <stdlib.h>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
 int main() {
-	system("Color 0B");
-	cout << "Welcome to SpaceOS. V.test A simulation" << "\n";
+	//preparing the main theme
+	system("color 0B");
+	
+	string OSinput;
+
 	while (true) {
+		std::cout << "->";
+		std::cin >> OSinput;
+
+
+		//commands for switch case function
+		int command_int = 0;
+		if (OSinput == "Prt") command_int = 1; //n
+		else if (OSinput == "Add") command_int = 2; //n
+		else if (OSinput == "Subs") command_int = 3; //n
+		else if (OSinput == "Mult") command_int = 4; //n
+		else if (OSinput == "Divi") command_int = 5;//n
+		else if (OSinput == "cr.file") command_int = 7; //n.f
+		else if (OSinput == "loop+") command_int = 8; //n
+		else if (OSinput == "loop++") command_int = 9; //n
+		else if (OSinput == "clear") command_int = 10; //dn
+		else if (OSinput == "sin") command_int = 11; // n until 40
+		else if (OSinput == "cos") command_int = 12;                              //////////////
+		else if (OSinput == "tan") command_int = 13;                             //random box//
+		else if (OSinput == "asin") command_int = 14;							///////////////
+		else if (OSinput == "acos") command_int = 15;
+		else if (OSinput == "atan") command_int = 16;
+		else if (OSinput == "sinh") command_int = 17;
+		else if (OSinput == "cosh") command_int = 18;
+		else if (OSinput == "tanh") command_int = 19;
+		else if (OSinput == "Version") command_int = 20; //dn
+		else if (OSinput == "company") command_int = 21; //dn
+		else if (OSinput == "Update") command_int = 22; //gbr (going to be removed)
+		else if (OSinput == "Info") command_int = 23;//dn
+
 		string a;
-		std::cin >> a;
-		if (a == "Prt") {
-			string a1;
-			std::cin >> a1;
-			std::cout << a1;
-		}
+		double ab, ba;
+		double bc, cb;
+		double cd, dc;
+		double ed, de;
+		string File_usage;
+		string looptest_1;
+		string looptest_2;
+		double Sina;
+		double Cosa;
+		double Tan_A;
+		double Asin;
+		double Acos;
+		double Atan;
+		double Sinh;
+		double Cosh;
+		double Tanh;
 
-		else if (a == "Oprt") {
-			int N1, N2;
-			std::cin >> N1;
-			std::cin >> N2;
-			std::cout << N1 + N2;
-		}
+		switch (command_int) {
+			case 1: 
+				std::cin >> a;
+				std::cout << a << "\n";
+			break;
+		
+			case 2: 
+				std::cout << "";
+				std::cin >> ab;
+				std::cin >> ba;
+				std::cout << ab + ba << "\n";
+				break;
 
-		else if (a == "Mult") {
-			int N3, N4;
-			std::cin >> N3;
-			std::cin >> N4;
-			std::cout << N3 * N4;
-		}
 
-		else if (a == "Divi") {
-			double N5, N6;
-			std::cin >> N5;
-			std::cin >> N6;
-			std::cout << N5 / N6;
-		}
-
-		else if (a == "Subs") {
-			int N7, N8;
-			std::cin >> N7;
-			std::cin >> N8;
-			std::cout << N7 - N8;
-		}
-
-		//------------------MATH -- done, return in 150 for SIN,cos...
-
-		else if (a == "loop++") {
-			string Lp;
-			std::cin >> Lp;
-			for (int i = 0; i < 10; i++) {
-				std::cout << Lp << "\n";
-			}
-			std::cout << "";
-		}
-
-		else if (a == "loop+") {
-			string Lp;
-			std::cin >> Lp;
-			for (int i = 0; i < 10; i++) {
-				std::cout << Lp;
-			}
-			std::cout << "";
-		}
-
-		//-----loops--done
-
-		else if (a == "cr.file") {
-
-			std::cout << "\n";
-
-			while (true) {
-				string File_name;
-				std::cin >> File_name;
-				if (File_name == "Quit.quit/command") {
+			case 3:
+				std::cin >> cb >> bc;
+				std::cout << cb - bc << "\n";
+				break;
+			
+			case 4:
+				std::cin >> cd >> dc;
+				std::cout << cd * dc << "\n";
+				break;
+			
+			case 5:
+				std::cin >> ed >> de;
+				std::cout << ed / de << "\n";
+				break;
+			
+			case 7:
+				while (true) {
+					std::cin >> File_usage;
+					if (File_usage == "Quit.quit/command") {
+						break;
+					}
 					break;
 				}
-
-				else if (File_name == "Enter.enter/command") {
-					string abcd;
-					std::cin >> abcd;
-					if (abcd == "LOL") {
-						std::cout << "Laughing Out Loud";
-					}
+			case 8:
+				std::cin >> looptest_1;
+				for (int io = 0; io > 10; io++) {
+					std::cout << looptest_1;
 				}
-			}
-			//--------------return in ~500-800
-
-
-		}
-
-		else if (a == "clear") {
-			system("cls");
-		}
-
-		//    clearscreen-done
-
-		else if (a == "shw.time") {
-			std::cout << 0;
-		}
-
-		else if (a == "caparezza") {
-			std::cout << "Sono fuori del tunnel(el-el-el) del divertimento (*2)" << "\n";
-			Sleep(11000);
-			std::cout << "Quando esco di casa mi annoio (~o), sono molto contento (~~o)" << "\n";
-			Sleep(4000);
-			std::cout << "Quando esco di casa mi annoio , sono molto piu^ contento (~~o)" << "\n";
-			Sleep(5500);
-			std::cout << "Di te che spendi" << "\n";
-			Sleep(1100);
-			std::cout << "stipendi stipato in posti" << "\n";
-			Sleep(1200);
-			std::cout << "stupendi tra culi su cubi" << "\n";
-			Sleep(1050);
-			std::cout << "succubi di beat orrendi";
-
-		}
-
-		else if (a == "Are-you-sure-?") {
-			std::cout << "pretty sure" << "\n";
-		}
-
-		else if (a == "what-?") {
-		std::cout << "are you sure ?" << "\n";
-		}
-
-		else if (a == "just-got-my-powers") {
-			std::cout << "Are you sure" << "\n";
-		}
-
-		else if (a == "pointer*") {
-			const char *s = "pointer-string";
-			std::cout << s << "\n";
-			while (true) {
-				string scd;
-				std::cin >> scd;
-				if (scd == "shwram-pointer") {
-					std::cout << &s << "\n";
+				break;
+			case 9:
+				std::cin >> looptest_2;
+				for (int io_2 = 0; io_2 > 10; io_2++) {
+					std::cout << looptest_2 << "n";
 				}
-			}
-		}
+				break;
+			case 10:
+				system("cls");
+				break;
 
-		else if (a == "e") {
-			std::cout << "tgduyclqe hgf";
-		
-		}
-		
-		else if (a == "cos") {
-			double cosF;
-			std::cin >> cosF;
-			std::cout << cos(cosF);
-		}
+			case 11:
+				std::cin >> Sina;
+				std::cout << sin(Sina) << "\n";
+				break;
 
-		else if (a == "sin") {
-			double sinB;
-			std::cin >> sinB;
-			std::cout << sin(sinB);
-		}
+			case 12:
+				std::cin >> Cosa;
+				std::cout << cos(Cosa) << "\n";
+				break;
+			
+			case 13: 
+				std::cin >> Tan_A;
+				std::cout << tan(Tan_A) << "\n";
+				break;
 
-		else if (a == "tan") {
-			double tanD;
-			std::cin >> tanD;
-			std::cout << tan(tanD);
-		}
+			case 14:
+				std::cin >> Asin;
+				std::cout << asin(Asin) << "\n";
+				break;
 
-		else if (a == "atan") {
-			double atana;
-			std::cin >> atana;
-			std::cout << atan(atana);
-		}
+			case 15:
+				std::cin >> Acos;
+				std::cout << acos(Acos) << "\n";
+				break;
 
-		else if (a == "asin") {
-			double asinb;
-			std::cin >> asinb;
-			std::cout << asin(asinb);
-		}
+			case 16:
+				std::cin >> Atan;
+				std::cout << atan(Atan) << "\n";
+				break;
 
-		else if (a == "acos") {
-			double acosc;
-			std::cin >> acosc;
-			std::cout << acos(acosc);
-		}
+			case 17:
+				std::cin >> Sinh;
+				std::cout << sinh(Sinh) << "\n";
+				break;
+			
+			case 18:
+				std::cin >> Cosh;
+				std::cout << cosh(Cosh) << "\n";
+				break;
 
-		else if (a == "tanh") {
-			double tanho;
-			std::cin >> tanho;
-			std::cout << tanh(tanho);
-		}
+			case 19:
+				std::cin >> Tanh;
+				std::cout << tanh(Tanh) << "\n";
+				break;
 
-		else if (a == "sinh") {
-			double sinhp;
-			std::cin >> sinhp;
-			std::cout << sinh(sinhp);
-		}
-		else if (a == "cosh") {
-			double coshq;
-			std::cin >> coshq;
-			std::cout << cosh(coshq);
-		}
+			case 20:
+				std::cout << "Version: alpha 0.a1" << "\n";
+				break;
+
+			case 21:
+				std::cout << "Xpak (XY PAcK)" << "\n";
+				break;
+
+			case 22:
+				std::cout << "no function yet" << "\n";
+				break;
+
+			case 23:
+				std::cout << "Owned by Xpak, version: alpha 0.a1\n"; //i forgor 
+				break;
+		}	
 	}
 }
